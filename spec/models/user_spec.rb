ff0fb7c4f1b95c8fd5d20271e20fe1f8f1b01403
Matheus Gois <mattheusgoiis@gmail.com>
2023-11-01 13:21:05 -0300

    feat: implement user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryBot.create(:user) }
+
+  it "is valid with valid attributes" do
+    expect(user).to be_valid
+  end
+
+  it "is not valid without an email" do
+    user.email = nil
+    expect(user).to_not be_valid
+  end
+
+  it "is not valid with a short password" do
+    user.password = FFaker::Lorem.characters(5)
+    expect(user).to_not be_valid
+  end
+
+  it "is valid with a password of minimum length" do
+    user.password = FFaker::Lorem.characters(6)
+    expect(user).to be_valid
+  end
+
 end
