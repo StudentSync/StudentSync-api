f6e13473bcfe78fb22b559aeb61c2d22b10f0978
dev-Gois <mattheusgoiis@gmail.com>
2023-11-07 11:02:16 -0300

    fix: solved user duplicated require in professor model

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -1,6 +1,6 @@
 class Professor < ApplicationRecord
   belongs_to :user
 
-  validates :user, uniqueness: true, presence: true
+  validates :user, uniqueness: true
   validates :name, presence: true
 end
