6d074d3005a3dc50894e811a00191edfc987fe28
dev-Gois <mattheusgoiis@gmail.com>
2023-11-25 00:57:47 -0300

    feat: add schedule_availability to professors

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_24_193228) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_25_035559) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -61,6 +61,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_24_193228) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "schedule_availability"
     t.index ["user_id"], name: "index_professors_on_user_id"
   end
 
